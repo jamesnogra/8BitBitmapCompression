e30200e63f13fcd94c8f00e00975065c923f68e3
jamesnogra <love.gerald@yahoo.com>
2017-07-30 16:23:04 +0800

    With huffman prefix codes.

---
+++ 8BitBitmapCompression/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace _8BitBitmapCompression
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.compressBtn = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(345, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.closeToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.openToolStripMenuItem.Text = "Open Image";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.closeToolStripMenuItem.Text = "Close";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox.Location = new System.Drawing.Point(13, 28);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(320, 240);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox.TabIndex = 1;
+            this.pictureBox.TabStop = false;
+            // 
+            // compressBtn
+            // 
+            this.compressBtn.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.compressBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.compressBtn.Location = new System.Drawing.Point(13, 275);
+            this.compressBtn.Name = "compressBtn";
+            this.compressBtn.Size = new System.Drawing.Size(320, 36);
+            this.compressBtn.TabIndex = 2;
+            this.compressBtn.Text = "Compress";
+            this.compressBtn.UseVisualStyleBackColor = false;
+            this.compressBtn.Click += new System.EventHandler(this.compressBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(345, 324);
+            this.Controls.Add(this.compressBtn);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "8-Bit Bitmap Compression";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button compressBtn;
+    }
+}
+
---
+++ 8BitBitmapCompression/Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _8BitBitmapCompression
+{
+    public partial class Form1 : Form
+    {
+        Bitmap mainImage;
+        List<ColorData> allColors;
+        List<HuffmanDictionary> allPrefixCodes;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            mainImage = new Bitmap(openFileDialog1.FileName);
+            pictureBox.Image = mainImage;
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Made by James Arnold E. Nogra");
+        }
+
+        private void compressBtn_Click(object sender, EventArgs e)
+        {
+            allColors = new List<ColorData>();
+            Color tempColor;
+
+            //iterate to the whole image (widthxheight)
+            for (int x=0; x<mainImage.Width; x++)
+            {
+                for (int y=0; y<mainImage.Height; y++)
+                {
+                    tempColor = mainImage.GetPixel(x, y);
+                    addColorOrIncreaseColorCount(tempColor.R);
+                    addColorOrIncreaseColorCount(tempColor.G);
+                    addColorOrIncreaseColorCount(tempColor.B);
+                }
+            }
+            allColors = allColors.OrderBy(o => o.count).ToList();
+            createHuffmanTree();
+            //MessageBox.Show(printAllIAllColors());
+        }
+
+        private void createHuffmanTree()
+        {
+            List<HuffmanTreeData> theTree = new List<HuffmanTreeData>();
+            HuffmanTreeData tempData, tempLeft, tempRight;
+
+            //FIRST put all the items in the allColors to the new list
+            foreach (ColorData e in allColors)
+            {
+                tempData = new HuffmanTreeData(e.colorCode, e.count, null, null, true);
+                theTree.Add(tempData);
+            }
+
+            if (allColors.Count>1)
+            {
+                //make the three
+                //extract the two items with the least count and make a new node
+                while (theTree.Count>1)
+                {
+                    //sort the treeData so that we can extract the least two
+                    theTree = theTree.OrderBy(o => o.totalCount).ToList();
+                    tempLeft = theTree.First(); //left of the new tree
+                    theTree.RemoveAt(0);
+                    tempRight = theTree.First(); //right of the new tree
+                    theTree.RemoveAt(0);
+                    tempData = new HuffmanTreeData((tempLeft.totalCount+tempRight.totalCount), tempLeft, tempRight, false);
+                    theTree.Add(tempData);
+                    //MessageBox.Show("Making new node with total count " + (tempLeft.totalCount + tempRight.totalCount) + "\nLeft Total: " + tempLeft.totalCount + "\nLeft Total: " + tempRight.totalCount);
+                }
+
+                //traverse the tree which is at the first element of the theTree
+                allPrefixCodes = new List<HuffmanDictionary>();
+                traverseHuffmanTreeForDictionary(theTree.First(), "");
+            }
+            else //what if there is only one byte data
+            {
+                HuffmanDictionary oneItem;
+                tempData = theTree.First();
+                oneItem = new HuffmanDictionary(tempData.item, "0", tempData.totalCount);
+                allPrefixCodes.Add(oneItem);
+            }
+            allPrefixCodes = allPrefixCodes.OrderByDescending(o => o.totalCount).ToList();
+            MessageBox.Show(printAllHuffmanCodes());
+        }
+
+        public void traverseHuffmanTreeForDictionary(HuffmanTreeData theTree, string currentPrefixCode)
+        {
+            HuffmanDictionary tempData;
+            if (theTree.isLeaf) //if this is the leaf, then we add to the list of Dictionary
+            {
+                tempData = new HuffmanDictionary(theTree.item, currentPrefixCode, theTree.totalCount);
+                allPrefixCodes.Add(tempData);
+                return;
+            }
+            traverseHuffmanTreeForDictionary(theTree.left, ("0" + currentPrefixCode));
+            traverseHuffmanTreeForDictionary(theTree.right, ("1" + currentPrefixCode));
+        }
+
+        private void addColorOrIncreaseColorCount(Byte singleColor)
+        {
+            bool isAlreadyInList = false;
+            foreach (ColorData eachColor in allColors)
+            {
+                if (singleColor == eachColor.colorCode)
+                {
+                    eachColor.count++;
+                    isAlreadyInList = true;
+                    break;
+                }
+            }
+            if (!isAlreadyInList)
+            {
+                ColorData newColorData = new ColorData(singleColor);
+                allColors.Add(newColorData);
+            }
+        }
+
+        private string printAllIAllColors()
+        {
+            string tempString = "Total Unique Colors: " + allColors.Count + "\n";
+            foreach (ColorData eachColor in allColors)
+            {
+                tempString += eachColor + "\n";
+            }
+            return tempString;
+        }
+
+        public string printAllHuffmanCodes()
+        {
+            string tempString = "All Huffman Codes\n\n";
+            foreach (HuffmanDictionary e in allPrefixCodes)
+            {
+                tempString += e + "\n";
+            }
+            return tempString;
+        }
+    }
+
+    public class HuffmanTreeData
+    {
+        public byte item;
+        public int totalCount;
+        public HuffmanTreeData left;
+        public HuffmanTreeData right;
+        public bool isLeaf;
+        public HuffmanTreeData(byte item, int totalCount, HuffmanTreeData left, HuffmanTreeData right, bool isLeaf)
+        {
+            this.item = item;
+            this.totalCount = totalCount;
+            this.left = left;
+            this.right = right;
+            this.isLeaf = isLeaf;
+        }
+        public HuffmanTreeData(int totalCount, HuffmanTreeData left, HuffmanTreeData right, bool isLeaf)
+        {
+            this.totalCount = totalCount;
+            this.left = left;
+            this.right = right;
+            this.isLeaf = isLeaf;
+        }
+    }
+
+    public class HuffmanDictionary
+    {
+        public byte item;
+        public string prefixCode;
+        public int totalCount;
+        public HuffmanDictionary(byte item, string prefixCode, int totalCount)
+        {
+            this.item = item;
+            this.prefixCode = prefixCode;
+            this.totalCount = totalCount;
+        }
+        public override string ToString()
+        {
+            return "Item: " + this.item + "\t\tTotal Count: " + this.totalCount + "\t\tPrefix Code: " + this.prefixCode;
+        }
+    }
+
+    public class ColorData
+    {
+        public byte colorCode;
+        public byte count;
+        public ColorData(byte colorCode)
+        {
+            this.colorCode = colorCode;
+            this.count = 1;
+        }
+        public override string ToString()
+        {
+            return "Color: " + this.colorCode + "\t\tCount: " + this.count;
+        }
+    }
+}
